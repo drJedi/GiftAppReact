3216d9e70f4e264f00e17d9b588e31b11216fa1b
Paweł Wilkosz <47307348+drJedi@users.noreply.github.com>
2025-09-23 15:51:33 +0200

    comi
    lekka zmiana w css i nadal praca

--- react-gift-finder/src/components/Header.jsx
+++ react-gift-finder/src/components/Header.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="header">
+    <main className="header">
       <h1 className="header__title">Znajdź idealny prezent dla najbliższych</h1>
       <p className="header__subtitle">
         Podaruj wyjątkowy, spersonalizowany prezent, inny niż wszystkie!
       </p>
-    </header>
+    </main>
   );
 };
 
